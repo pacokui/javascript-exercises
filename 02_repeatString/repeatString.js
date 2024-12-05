735dfbafaadb432b926105f1270ff4cfe3944d9e
pacokui <pacokui@github.com>
2024-12-05 11:48:51 +0100

    solved exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let result = '';
+    for (let i = 0; i <num; i++) {
+        result += str;
+    }
+    if (num < 0) {
+        return ('ERROR');
+    } 
+    return result;
+         
 };
 
 // Do not edit below this line
