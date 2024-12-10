bf116ea084b186940c766d6f045e76ae53ec3662
pacokui <pacokui@github.com>
2024-12-10 11:01:26 +0100

    solved reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    const arr =  str.split('')
+    arr.reverse();
+    return arr.join('');
 };
-
 // Do not edit below this line
 module.exports = reverseString;
