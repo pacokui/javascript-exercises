75f597fb5b0bd36b15ce38f8e6974d6fecfb68bc
pacokui <pacokui@github.com>
2025-01-02 23:39:18 +0100

    solved fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,33 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    let num1 = 1;
+    let num2 = 1;
+    let sum;
+    let parsed;
+    if (typeof num !== "number"){
+        parsed = parseInt(num)
+
+    } else { 
+        parsed = num}
+    
+    if (parsed === 1) {
+        return num1;
+    } else if (parsed === 0){
+        return 0;
+    }
+    else if (parsed === 2) {
+        return num2;
+    } else if (parsed < 0 ) {
+        return "OOPS";
+    }
+    else {
+        for (let i = 3; i <= parsed; i++) {
+            sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+        }
+        return num2;
+    }
 };
 
 // Do not edit below this line
